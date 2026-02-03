17164d7d829cbdb9a061f83ad7b868c47123be95
Willian Anderson <willian01314551@gmail.com>
2026-02-03 01:25:58 -0300

    feat(models): adicionar models iniciais do projeto

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public string Nome { get; set; }
 
-
         public List<Produto> Produtos { get; set; }
     }
 }
--- Models/ItensPedido.cs
+++ Models/ItensPedido.cs
@@ -1,6 +1,20 @@
-﻿namespace ProjetoTechStore_Volvo_2026.Models
+﻿using System.Text.Json.Serialization;
+
+namespace ProjetoTechStore_Volvo_2026.Models
 {
-    public class ItensPedido
+    public class ItemPedido
     {
+        public int Id { get; set; }
+
+        // FK para Pedido
+        public int PedidoId { get; set; }
+        public Pedido Pedido { get; set; }
+
+        // FK para Produto
+        public int ProdutoId { get; set; }
+        public Produto Produto { get; set; }
+
+        public int Quantidade { get; set; }
+        public decimal PrecoUnitario { get; set; }
     }
 }
