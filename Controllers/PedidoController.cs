df5b19158af9d40f252c8dcc4395c7a12cfa5147
Felipe Mossato <Felipemossato25@gmail.com>
2026-02-05 19:16:21 -0300

    Adicionado: comentários no controller.

--- Controllers/PedidoController.cs
+++ Controllers/PedidoController.cs
@@ -17,7 +17,12 @@ namespace ProjetoTechStore_Volvo_2026.Controllers
             _Service = _svc;
         }
 
-
+        //Metodo listar pedidos lista todos os pedidos em formato de DTO:
+        /*
+            é retornado um pedidorespostadto que contem: id, nomecliente, data do pedido, valor total do pedido e seu status.
+            Além disso é retornado todos os items associados ao pedido com ujma classe item pedido que contem:
+            nome do produto, quantidade e valor unitario.
+        */
         [HttpGet]
         public ActionResult<List<PedidoRespostaDTO>> ListarPedidos()
         {
@@ -31,8 +36,8 @@ namespace ProjetoTechStore_Volvo_2026.Controllers
                 return StatusCode(500, new {message = "Erro ao listar pedidos."});
             }
         }
-
-        [HttpGet("{pedidoIdd}")]
+        //Mesmo retorno do listarpedidos, mas apenas lista um pedido procurado por id.
+        [HttpGet("{pedidoId}")]
         public ActionResult<PedidoRespostaDTO>? ProcurarPedidoPorID(int pedidoId)
         {
             var pedido = _Service.ProcurarPedidoPorID(pedidoId);
@@ -43,6 +48,7 @@ namespace ProjetoTechStore_Volvo_2026.Controllers
             return Ok(pedido);
         }
 
+        //Permite a criação de pedidos e da adição de produtos.
         [HttpPost]
         public ActionResult<PedidoCriarDTO> CriarPedido([FromBody] PedidoCriarDTO pedido)
         {
@@ -53,7 +59,7 @@ namespace ProjetoTechStore_Volvo_2026.Controllers
             }
             return CreatedAtAction($"Pedido com o id: ", new {id = pedido.Id}, pedido);
         }
-
+        //atualiza o status do pedido.
         [HttpPut("{pedidoId}")]
         public ActionResult<PedidoRespostaDTO> UpdateStatusPedido(int pedidoId, StatusPedido _status)
         {
@@ -65,7 +71,7 @@ namespace ProjetoTechStore_Volvo_2026.Controllers
 
             return Ok();
         }
-
+        //Deleta o pedido, apenas aqui por questões de debug pois não acho correto a deleção de pedidos.
         [HttpDelete("pedidoId")]
         public IActionResult DeletarPedidoPorID(int pedidoId)
         {
