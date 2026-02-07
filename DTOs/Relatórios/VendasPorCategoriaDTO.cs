f36f1df44ccd291c00d922ba942c0d84ec854cf4
Felipe Mossato <Felipemossato25@gmail.com>
2026-02-07 20:26:58 -0300

    Add DTO temporario das vendas

--- DTOs/Relatórios/VendasPorCategoriaDTO.cs
+++ DTOs/Relatórios/VendasPorCategoriaDTO.cs
@@ -1,6 +1,14 @@
-﻿namespace ProjetoTechStore_Volvo_2026.DTOs.Relatórios
+﻿using ProjetoTechStore_Volvo_2026.Models;
+
+namespace ProjetoTechStore_Volvo_2026.DTOs.Relatórios
 {
     public class VendasPorCategoriaDTO
     {
+        public int CategoriaId;
+        public required Categoria Categoria;
+
+        public decimal ValorTotal; //Calculado na hora da criação
+        //public decimal ValorMedio;
+
     }
 }
