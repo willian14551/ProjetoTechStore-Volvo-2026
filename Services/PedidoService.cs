41c45897df64524f3619adbe1fb662a0c5098328
Felipe Mossato <Felipemossato25@gmail.com>
2026-02-09 20:10:35 -0300

    Adição de comentários

--- Services/PedidoService.cs
+++ Services/PedidoService.cs
@@ -22,13 +22,20 @@ public class PedidoService
 
     public async Task<Pedido> CriarPedido(PedidoEntradaDTO pedido)
     {
+        //utiliza-se de um DTO de entrada para criação, pois não é necessário ter todos os parâmetros sendo passados para o backend.
+        //utiliza-se da estratégia para que o banco de dados possa fazer as operações caso ocorra a necessidade de um retry.
         var estrategiaExecucao = _context.Database.CreateExecutionStrategy();
+        //metodo de retorno com todas as funções:
         return await estrategiaExecucao.ExecuteAsync(async () =>
         {
+            //Começa a transação para que o banco de dados faça tudo de uma vez só.
+            //metodo using para que o try catch fique mais otimizado, retornando quando deve e realizando Dispose() se necessário.
+
             using var transacao = await _context.Database.BeginTransactionAsync();
+            //Tenta criar o pedido
             try
             {
-                
+                //Pedido criado dessa forma pois temos atributos required.
                 Pedido ped = new Pedido
                 {
                     NomeCliente = pedido.NomeCliente,
@@ -37,22 +44,24 @@ public class PedidoService
                     Itens = new List<ItemPedido>()
                 };
 
-
+                //Roda a lista de pedidos, pois cada pedido pode ter mais de um produto envolvido.
                 foreach(var produtoAux in pedido.Itens)
                 {
                     var produto = await _context.Produtos.FindAsync(produtoAux.ProdutoId);
+                    //Se não achou o produto, joga uma exceção.
                     if(produto == null)
                     {
                         throw new Exception($"O produto com o id:{produtoAux.ProdutoId} não foi encontrado.");
                     }
-
+                    //Exceção de erro de estoque.
                     if (produto.Estoque < produtoAux.Quantidade)
                     {
                         throw new Exception($"{produto.Nome} não possui estoque suficiente para realizar a transação.");
                     }
 
+                    //reduz a quantidade.
                     produto.Estoque-=produtoAux.Quantidade;
-
+                    //cria o item pedido, classe necessária para a relação entre pedido e produto.
                     var NovoItemPedido = new ItemPedido
                     {
                         ProdutoId = produto.Id,
@@ -61,13 +70,18 @@ public class PedidoService
                     };
                     ped.Itens.Add(NovoItemPedido);
                 }
+                //Após a criação do pedido, adiciona ao banco de dados e salva de forma assincrona.
                 _context.Pedidos.Add(ped);
+                //salva as mudanças realizadas no banco de dados.
                 await _context.SaveChangesAsync();
+                //finaliza a transação.
                 await transacao.CommitAsync();
+                //retorna o pedido para realização de operações no controller.
                 return ped;
             }
             catch(Exception ex)
             {
+                //caso dê erro, faça um rollback no banco de dados e exibe uma mensagem de erro.
                 await transacao.RollbackAsync();
                 var mensagemErro = ex.InnerException != null ? ex.InnerException.Message : ex.Message;
                 throw new Exception($"Erro ao criar o pedido: {mensagemErro}.");
@@ -77,27 +91,29 @@ public class PedidoService
 
     public async Task<List<PedidoRespostaDTO>> ListarPedidos()
     {
+        //lista os pedidos de forma assincrona, utiliza-se do asnotracking pois é um método apenas de leitura.
         return await _context.Pedidos
         .AsNoTracking()
-        .OrderByDescending(p => p.DataPedido)
-        .Select(p => new PedidoRespostaDTO
+        .OrderByDescending(p => p.DataPedido) //ordena por ordem decrescente.
+        .Select(p => new PedidoRespostaDTO //seleciona os pedidos e os converte em pedido resposta, necessário pois não queremos mostrar todas as informações do pedido.
         {
             Id = p.Id,
             DataPedido = p.DataPedido,
             NomeCliente = p.NomeCliente,
             ValorTotal = p.Itens.Sum(i => i.PrecoUnitario * i.Quantidade),
             stt = p.Status,
-            Itens = p.Itens.Select(item => new ItemPedidoRespostaDTO
+            Itens = p.Itens.Select(item => new ItemPedidoRespostaDTO //mesma ideia para item pedido, convertemos em dto de resposta.
             {
                 Quantidade = item.Quantidade,
                 PrecoUnitario = item.PrecoUnitario,
                 NomeProduto = item.Produto.Nome
-            }).ToList()
-        }).ToListAsync();
+            }).ToList() //to list para criar listas..
+        }).ToListAsync(); 
     }
 
     public async Task<PedidoRespostaDTO?> ProcurarPedidoPorID(int pedidoId)
     {
+        //mesma ideia do método anterior, apeans difere-se pela pesquisa por id e não listagem geral.
         return await _context.Pedidos
         .AsNoTracking()
         .Where(p => p.Id == pedidoId)
@@ -114,13 +130,14 @@ public class PedidoService
                 Quantidade = item.Quantidade,
                 PrecoUnitario = item.PrecoUnitario
 
-            }).ToList()
+            }).ToList() //to list para criar listas..
         }).FirstOrDefaultAsync();
 
     }
 
     public async Task<bool> UpdateStatusPedido(int pedidoId, StatusPedido stt)
     {
+        //este método apenas atualiza o status do pedido, pois atualizar o pedido em si não é necessário.
         var pedido = await _context.Pedidos.FindAsync(pedidoId);
         if(pedido == null)
         {
@@ -138,13 +155,15 @@ public class PedidoService
 
     //Em um negócio não faz sentido deletar os pedidos, mesmo que sejam pedidos fraudulentos ou pedidos errados, mas está aqui por método de debug. 
     public async Task<bool> DeletarPedidoPorID(int pedidoId)
-    {
+    {   
+        //faz a pesquisa do pedido por id:
         var pedido = await _context.Pedidos
         .Include(p => p.Itens)
         .FirstOrDefaultAsync(p => p.Id == pedidoId);
 
         if(pedido == null) { return false; }
 
+        //vai de item em item retornando o estoque, pois o pedido está "cancelado"
         foreach(var Item in pedido.Itens)
         {
             var produto = await _context.Produtos.FindAsync(Item.ProdutoId);
@@ -153,11 +172,14 @@ public class PedidoService
                 produto.Estoque+= Item.Quantidade;
             }
         }
+
         _context.Pedidos.Remove(pedido);
+        //salva as mudanças realizadas.
         await _context.SaveChangesAsync();
         return true;
     }
 
+    //faz uma conversão, método auxiliar.
     public PedidoRespostaDTO ConverterParaDTO(Pedido pedido)
     {
         return new PedidoRespostaDTO
